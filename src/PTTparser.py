ff1c425b0b216764678df022f2139121ac0565f2
OEG <oeg8168@gmail.com>
2016-12-05 20:40:06 +0800

    code refactoring: separate parseArticle()

--- src/PTTparser.py
+++ src/PTTparser.py
@@ -22,42 +22,47 @@ class PTTparser:
         html = response.content.decode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
 
-        metas = soup.find_all('span', class_='article-meta-value')
-        # metas = soup.find_all('span', {'class': 'article-meta-value'})
-
-        author = metas[0].text
-        userID = self.getUserID(author)
-        boardName = metas[1].text
-        title = metas[2].text
-        postTime = metas[3].text
-
-        print('Author: ' + author)
-        print('User ID: ' + userID)
-        print('Board Name: ' + boardName)
-        print('Title: ' + title)
-        print('Post Time: ' + postTime)
-
-        pushs = soup.find_all('div', class_='push')
-        messages = []
-        for p in pushs:
-            pushTag = p.find('span', class_='push-tag').text
-            pushUserID = p.find('span', class_='push-userid').text
-            pushContent = p.find('span', class_='push-content').text[2:]
-
-            messages.append({'pushTag': pushTag, 'pushUserID': pushUserID, 'pushContent': pushContent})
-            # print(p.text)
+        metas = self.getArticleMeta(soup)
+        allPushs = self.getAllPushs(soup)
 
         articleJSON = {
-            'author': author,
-            'userID': userID,
+            'authorID': self.getArticleAuthorID(metas),
             'boardName': boardName,
-            'title': title,
-            'postTime': postTime,
-            'messages': messages
+            'title': self.getArticleTitle(metas),
+            'postTime': self.getArticlePostTime(metas),
+            'pushMessages': self.getArticlePushMessages(allPushs)
         }
 
         print('=====JSON=====')
-        print(json.dumps(articleJSON, sort_keys=True, indent=4, ensure_ascii=False))
+        jsonText = json.dumps(articleJSON, sort_keys=True,
+                              indent=4, ensure_ascii=False)
+        print(jsonText)
 
-    def getUserID(self, author):
-        return author.split(' ')[0]
+    def getArticleMeta(self, soup):
+        return soup.find_all('span', class_='article-meta-value')
+        # return soup.find_all('span', {'class': 'article-meta-value'})
+
+    def getAllPushs(self, soup):
+        return soup.find_all('div', class_='push')
+
+    def getArticleAuthorID(self, metas):
+        return metas[0].text.split(' ')[0]
+
+    def getArticleTitle(self, metas):
+        return metas[2].text
+
+    def getArticlePostTime(self, metas):
+        return metas[3].text
+
+    def getArticlePushMessages(self, allPushs):
+        pushMessages = []
+        for p in allPushs:
+            message = {
+                'pushTag': p.find('span', class_='push-tag').text[0],
+                'pushUserID': p.find('span', class_='push-userid').text,
+                'pushContent': p.find('span', class_='push-content').text[2:]
+            }
+
+            pushMessages.append(message)
+
+        return pushMessages
