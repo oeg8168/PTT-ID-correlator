f571867059f3a4dfd20fe723aa4a29cf5ac0aa15
OEG <oeg8168@gmail.com>
2016-12-10 21:17:41 +0800

    update PTTcrawler main frame

--- src/PTTcrawler.py
+++ src/PTTcrawler.py
@@ -4,7 +4,14 @@ from src.PTTparser import PTTparser
 class PTTcrawler:
 
     def __init__(self):
+        self.databasePath = './database/'
         self.pttParser = PTTparser()
 
     def crawl(self):
+        pass
+
+    def updateDatabase(self):
+        pass
+
+    def createDatabase(self):
         pass
