cc4c7f3964fb150a59bc16236a1fe7f2aa326c56
OEG <oeg8168@gmail.com>
2016-12-10 16:15:29 +0800

    code refactoring: separate parseBoard()

--- src/PTTparser.py
+++ src/PTTparser.py
@@ -12,25 +12,26 @@ class PTTparser:
 
     def parseBoard(self, boardName, pagesToBeParsed=100):
         boardURL = self.PTTaddress + boardName + '/index.html'
-        # TODO get latest
         print(boardURL)
 
         response = requests.get(boardURL)
         html = response.content.decode('utf-8')
         soup = BeautifulSoup(html, 'html.parser')
 
-        pagingButtons = soup.find_all('a', class_='btn wide')
+        latestPageNum = self.getLatestPageNum(soup)
 
+        parsePageNum = latestPageNum
+        for i in range(pagesToBeParsed):
+            print('page: ' + str(parsePageNum))
+            parsePageNum -= 1
+            self.parsePage('Tainan', parsePageNum)
+
+    def getLatestPageNum(self, soup):
+        pagingButtons = soup.find_all('a', class_='btn wide')
         latestPageLink = pagingButtons[1].get('href')
         latestPageNum = re.search('\d+', latestPageLink).group(0)
 
-        latestPageNum = int(latestPageNum)
-        parsePageNum = latestPageNum
-        for i in range(pagesToBeParsed):
-            print(i)
-            print(parsePageNum)
-            parsePageNum -= 1
-            self.parsePage('Tainan', parsePageNum)
+        return int(latestPageNum)
 
     def parsePage(self, boardName, pageNum):
         pageIndex = '/index' + str(pageNum) + '.html'
